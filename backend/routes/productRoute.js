a09ff90e14c0ee28e046ec3b16725162018ece89
Sabbaghh <afsabbagh9@gmail.com>
2021-07-23 21:19:00 +0300

    Implementing addingProductReview & getTopproducts

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,6 +5,8 @@ import {
 	deleteProductById,
 	createProduct,
 	updateProduct,
+	createProductReview,
+	getTopProducts,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
@@ -14,5 +16,8 @@ router.route('/:id').get(getProductById);
 router.route('/:id').delete(protect, admin, deleteProductById);
 router.route('/').post(protect, admin, createProduct);
 router.route('/:id').put(protect, admin, updateProduct);
+router.route('/:id').put(protect, admin, updateProduct);
+router.route('/:id/reviews').put(protect, createProductReview);
+router.route('/top').put(protect, getTopProducts);
 
 export default router;
